754290a04f01c6bc9364e644ceed4a0ba368ab9d
marekgagne <marekgagne@gmail.com>
2026-03-17 14:50:56 -0400

    document

--- orbite-lune/Europe.gd
+++ orbite-lune/Europe.gd
@@ -1,14 +1,18 @@
 extends RigidBody3D
 
-@export_group("variables orbite")
-@export var masse_europe: float
-@export var temps_orbit: float
-@export var masse_jupiter: float
+@export var masse_europe: float 
+var temps_orbit: float = 299.819e3
+var masse_jupiter: float = 1.989e27
 @export var r_p: float
-@export var v_p: float
-@export var k:float
-@export var d:float
-@export var coef_dissip:float
+@export var v_p: float 
+var k:float = 1e14
+var d:float = 249.7e6
+var coef_dissip:float = 4e16
+@export var centre_de_rotation: RigidBody3D
+
+
+var G : float = 6.673e-11
+var position_relle:float
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,3 +23,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+func calculer_acceleration_gravitationnelle(position_rellee: Vector3) -> Vector3:
+	var facteur = -G * masse_europe * masse_jupiter / (position_rellee.length()**3)
+	var force = (position_rellee - .position) * facteur
