f9292d0c5882c2368fe2e9db28f5faac7f52aa57
marekgagne <marekgagne@gmail.com>
2026-03-17 13:55:31 -0400

    document

--- orbite-lune/Europe.gd
+++ orbite-lune/Europe.gd
@@ -1,5 +1,15 @@
 extends RigidBody3D
 
+@export_group("variables orbite")
+@export var masse_europe: float
+@export var temps_orbit: float
+@export var masse_jupiter: float
+@export var r_p: float
+@export var v_p: float
+@export var k:float
+@export var d:float
+@export var coef_dissip:float
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
