a849b219b579a093756d4f0a6304af54d294d1f5
Antoine Lapointe <antoinelapointe2007@gmail.com>
2026-04-22 14:37:44 -0400

    Mouvement caméra

--- systeme-solaire/déplacement.gd
+++ systeme-solaire/déplacement.gd
@@ -1,6 +1,8 @@
 extends Camera3D
 var pos_initiale = Vector3(3,3,3)
-var avant_camera = -global_transform.basis.z
+var vitesse = 10
+var vitesse_rotation = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = pos_initiale 
@@ -8,14 +10,48 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("déplacement devant"):
-		avancer()
-	if Input.is_action_just_pressed("déplacement arrière"):
-		avancer()
+	#Déplacements
+	if Input.is_action_pressed("déplacement devant"):
+		avancer(delta)
+	if Input.is_action_pressed("déplacement arrière"):
+		reculer(delta)
+	if Input.is_action_pressed("déplacement droite"):
+		droite(delta)
+	if Input.is_action_pressed("déplacement gauche"):
+		gauche(delta)
+		
+	#Rotation
+	if Input.is_action_pressed("rotation gauche"):
+		rotation_gauche(delta)
+	if Input.is_action_pressed("rotation droite"):
+		rotation_droite(delta)
+		
+	#Inclinaison
+	if Input.is_action_pressed("Inclinaison bas"):
+		inclinaison_bas(delta)
+	if Input.is_action_pressed("inclinaison haut"):
+		inclinaison_haut(delta)
+		
+func avancer(delta):
+	position -= global_transform.basis.z * vitesse * delta
 
+func reculer(delta):
+	position += global_transform.basis.z * vitesse * delta
 
-func avancer() -> void :
-	position += avant_camera
+func droite(delta):
+	position += global_transform.basis.x * vitesse * delta
+
+func gauche(delta):
+	position -= global_transform.basis.x * vitesse *  delta
+
+func rotation_gauche(delta):
+	rotate_y(vitesse_rotation * delta)
 	
-func reculer() -> void :
-	position -= avant_camera
+func rotation_droite(delta):
+	rotate_y(-vitesse_rotation * delta)
+	
+func inclinaison_bas(delta):
+	rotate_x(-vitesse_rotation * delta)
+	
+func inclinaison_haut(delta):
+	rotate_x(vitesse_rotation * delta)
